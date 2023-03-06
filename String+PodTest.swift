1b019d69b6eb31907eab1a2b9844d84dcb0d6452
mrsong <424607870@qq.com>
2023-03-06 17:48:24 +0800

    update

---
+++ String+PodTest.swift
@@ -0,0 +1,15 @@
+//
+//  String+PodTest.swift
+//  velo-ios
+//
+//  Created by MrSong on 2023/3/6.
+//  Copyright © 2023 East West Bank. All rights reserved.
+//
+
+import Foundation
+
+public extension String {
+    var testPodVersion: String {
+        "\(self) version 0.0.1"
+    }
+}
